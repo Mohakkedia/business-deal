165c0c55bc1c27355bee24f8fc63dbf7894177b6
Mohakkedia <mvk240000@utdallas.edu>
2026-07-22 14:47:36 -0500

    Bind server to 0.0.0.0 for reliable dual IPv4/IPv6 connections

--- server.js
+++ server.js
@@ -29,6 +29,6 @@ app.get('/room/:code', (req, res) => {
 initializeSocketHandlers(io, roomManager);
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
+server.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
